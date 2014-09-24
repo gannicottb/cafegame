ca551afae3696aa0633dadeee8a8a08694d5f582
Yanna Wu <wuyanna2009@gmail.com>
2014-09-24 15:19:54 -0700

    put trendinglist on right screen

--- guess_who/js/large-left.js
+++ guess_who/js/large-left.js
@@ -98,45 +98,45 @@ function main(session){
 
    // Subscribe to trending guesses
    // 
-   session.subscribe("com.google.guesswho.onguess",
-      function(args) {
-         var event = args[0];
-         //console.log("ON GUESS !!!!!!!!!!!!!!!!!!!!!!!");      
-         console.log(event);      
+  //  session.subscribe("com.google.guesswho.onguess",
+  //     function(args) {
+  //        var event = args[0];
+  //        //console.log("ON GUESS !!!!!!!!!!!!!!!!!!!!!!!");      
+  //        console.log(event);      
 
-         printGuesses(event.guesses);
+  //        printGuesses(event.guesses);
 
-         //$('#guessList').append("<p>"+event.guess+" from " + event.user);   
-         //document.getElementById("guessList").getElementsByTagName("span").value = event.user + "from" + event.guess;
-      });
+  //        //$('#guessList').append("<p>"+event.guess+" from " + event.user);   
+  //        //document.getElementById("guessList").getElementsByTagName("span").value = event.user + "from" + event.guess;
+  //     });
 
-  var printGuesses = function(guesses)
-  {
-    //Create a list item
-    var list = document.createElement('ul');
+  // var printGuesses = function(guesses)
+  // {
+  //   //Create a list item
+  //   var list = document.createElement('ul');
 
-    for(var i = 0; i < guesses.length; i++) {
+  //   for(var i = 0; i < guesses.length; i++) {
 
-        console.log(guesses[i]);
+  //       console.log(guesses[i]);
 
-        // Create the list item:
-        var item = document.createElement('li');
+  //       // Create the list item:
+  //       var item = document.createElement('li');
 
-        // Set its contents:
-        item.appendChild(document.createTextNode(guesses[i].name));
+  //       // Set its contents:
+  //       item.appendChild(document.createTextNode(guesses[i].name));
 
-        // Add it to the list:
-        list.appendChild(item);
-    }
+  //       // Add it to the list:
+  //       list.appendChild(item);
+  //   }
 
-    var listElement = document.getElementById('guessList');
+  //   var listElement = document.getElementById('guessList');
 
-    if(listElement.hasChildNodes() === true)
-      listElement.removeChild(listElement.childNodes[0]);
+  //   if(listElement.hasChildNodes() === true)
+  //     listElement.removeChild(listElement.childNodes[0]);
     
-    listElement.appendChild(list);
+  //   listElement.appendChild(list);
     
-  }
+  // }
 
 
   // Pixelate
