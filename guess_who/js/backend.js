b98f4c3883a24bd55ca7715286aa68c1916c60c8
Brandon Gannicott <brandon.gannicott@gmail.com>
2014-09-30 19:24:03 -0700

    forgot to run the files through JSFormat. handy sublime tool for formatting

--- guess_who/js/backend.js
+++ guess_who/js/backend.js
@@ -1,13 +1,13 @@
-var Backend = (function () {
+var Backend = (function() {
 
   //Private variables
   //
 
   //Constants
-  var NUMBER_OF_ANSWERS= 4;
-  var ROUND_DURATION= 20000; // in ms
-  var MIN_PLAYERS_TO_START= 2; //set to 2 for DEBUG
- 
+  var NUMBER_OF_ANSWERS = 4;
+  var ROUND_DURATION = 20000; // in ms
+  var MIN_PLAYERS_TO_START = 2; //set to 2 for DEBUG
+
   // Members
   var session;
   var correct_answer = null;
@@ -21,24 +21,24 @@ var Backend = (function () {
   var round_end = 0;
 
   //Private methods
-  verify = function(user){
-    if(user == undefined || user == null || user.logged_in == false){
-        //the user isn't registered or logged in
-        //throw an error of some kind
-        throw new autobahn.Error('com.google.guesswho.error', ["User isn't registered or logged in"], user);
+  verify = function(user) {
+    if (user == undefined || user == null || user.logged_in == false) {
+      //the user isn't registered or logged in
+      //throw an error of some kind
+      throw new autobahn.Error('com.google.guesswho.error', ["User isn't registered or logged in"], user);
     }
   };
 
-  lookup = function(uid){
+  lookup = function(uid) {
     return users[Number(uid)];
   };
 
   //+ Jonas Raoni Soares Silva
   //@ http://jsfromhell.com/array/shuffle [v1.0]
-  shuffle = function(o){ //v1.0
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  shuffle = function(o) { //v1.0
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
-  };  
+  };
 
   // Register new devices
   //
@@ -54,13 +54,13 @@ var Backend = (function () {
 
   // Change user names
   //
-  changeName = function(args, kwargs, details){
+  changeName = function(args, kwargs, details) {
     var user = lookup(args[0]);
     var new_name = args[1];
-    
+
     verify(user); // throw an error if the user doesn't exist
 
-    console.log("User "+user.id+" changed their name to "+new_name);
+    console.log("User " + user.id + " changed their name to " + new_name);
     user.name = new_name;
 
     return user.name; //receipt
@@ -71,7 +71,7 @@ var Backend = (function () {
   //
   submitGuess = function(args, kwargs, details) {
     // Check to make sure the round is still going
-    if(round_in_progress === false){
+    if (round_in_progress === false) {
       return;
     }
 
@@ -84,10 +84,10 @@ var Backend = (function () {
 
     // Determine their score, add it to their total
     var score = 0;
-    
-    if(correct){ // If you didn't get it right, you get a score of 0 (maybe some very small number just for playing?)
+
+    if (correct) { // If you didn't get it right, you get a score of 0 (maybe some very small number just for playing?)
       var time_left = Math.floor((round_end - kwargs.time)); // in ms
-      score = ((time_left/ROUND_DURATION) * 5) | 0; // Max score is 5.   
+      score = ((time_left / ROUND_DURATION) * 5) | 0; // Max score is 5.   
     }
 
     user.score += score;
@@ -100,43 +100,46 @@ var Backend = (function () {
     })
 
     // Return their score for the round
-    result = {correct: correct, score: score}
+    result = {
+      correct: correct,
+      score: score
+    }
     return result;
   };
 
   // When a user logs out
   //
-  onLogout = function(args, kwargs, details){
+  onLogout = function(args, kwargs, details) {
     var user = lookup(args[0]);
     user.logged_in = false;
     // Just in case something weird has happened. We can't have a negative number of users.
-    if(logged_in_users > 0) logged_in_users--;
-    var logout_msg = 'User '+user.name+' has logged out!';
+    if (logged_in_users > 0) logged_in_users--;
+    var logout_msg = 'User ' + user.name + ' has logged out!';
     console.log(logout_msg);
     return logout_msg;
   };
 
   // Begin the round
   //
-  startNextRound = function(){
+  startNextRound = function() {
     // Increment (wrapping if at end of list) the round
-    round = (round+1)%guess_list.length;
+    round = (round + 1) % guess_list.length;
     round_in_progress = true;
 
-    $('#round_number').html($('#round_number').html()+" "+round);
-    
+    $('#round_number').html($('#round_number').html() + " " + round);
+
     //Pick the keyword for the round, save the id
     //correct_id = guess_list[round].id;
     correct_answer = guess_list[round];
-    
+
     //Generate the answers
     //
     //Slice the list of keywords before and after the current keyword, then glue them together and shuffle the result
-    var potentialAnswers = shuffle(guess_list.slice(0,round).concat(guess_list.slice(round + 1,guess_list.length)));    
+    var potentialAnswers = shuffle(guess_list.slice(0, round).concat(guess_list.slice(round + 1, guess_list.length)));
     // clear out the answers  
-    answers = [];   
+    answers = [];
     //load in the correct answer
-    answers[0] = correct_answer; 
+    answers[0] = correct_answer;
     // concatenate a slice of more possible answers to the array
     answers = answers.concat(potentialAnswers.slice(0, NUMBER_OF_ANSWERS - 1));
     // randomize the answer choices
@@ -157,53 +160,59 @@ var Backend = (function () {
 
   // When the round timeout is reached
   //
-  onRoundOver = function(args, kwargs, details){
+  onRoundOver = function(args, kwargs, details) {
     round_in_progress = false;
     round_end = 0;
     //TODO:
     // Grab the top X highest scoring players and put their info into an object
     // Publish that leaderboard object for the large-right display
-    
-    session.publish('com.google.guesswho.roundEnd',[], {round: round, answers: correct_answer});
 
-    if (round_in_progress === false && logged_in_users >= MIN_PLAYERS_TO_START){
+    session.publish('com.google.guesswho.roundEnd', [], {
+      round: round,
+      answers: correct_answer
+    });
+
+    if (round_in_progress === false && logged_in_users >= MIN_PLAYERS_TO_START) {
       //Start the next round in 5 seconds
       setTimeout(startNextRound, 5000);
     }
   };
 
   // Login new and existing users
-  login = function(args, kwargs, details) {    
-    var uid = args[0];//it's a string
-    console.log("uid "+args[0]+" logging in");
+  login = function(args, kwargs, details) {
+    var uid = args[0]; //it's a string
+    console.log("uid " + args[0] + " logging in");
 
     // Register if the user passes in a null id
-    if (uid == null || uid == undefined){
+    if (uid == null || uid == undefined) {
       uid = register();
     }
 
     // Grab the user from the "database"
-    var user = lookup(uid);    
+    var user = lookup(uid);
     // Log them in
     user.logged_in = true;
-    logged_in_users++;      
-    
-    if (round_in_progress == false && logged_in_users >= MIN_PLAYERS_TO_START){
+    logged_in_users++;
+
+    if (round_in_progress == false && logged_in_users >= MIN_PLAYERS_TO_START) {
       //Start the next round in 5 seconds
       setTimeout(startNextRound, 5000);
     }
 
     session.publish("com.google.guesswho.newLogin", [], {
       players_needed: MIN_PLAYERS_TO_START - logged_in_users,
-      new_player: {id: user.id, name: user.name}
+      new_player: {
+        id: user.id,
+        name: user.name
+      }
     });
 
-    console.log("User "+user.name+" is logged in.");
+    console.log("User " + user.name + " is logged in.");
 
     return user;
   };
 
-  main = function(autobahn_session){
+  main = function(autobahn_session) {
 
     session = autobahn_session;
     //Get the curated list of people
@@ -212,14 +221,17 @@ var Backend = (function () {
       var lines = myContentFile.split("\n");
       for (var i = 0; i < lines.length; i++) {
         //save in object "guesslist": 
-        guess_list[i] = {id: i, keyword: lines[i]}
+        guess_list[i] = {
+          id: i,
+          keyword: lines[i]
+        }
         console.log(guess_list[i].id, guess_list[i].keyword);
       }
       console.log("guess_list.length =" + guess_list.length);
       //Shuffle the keywords in the list
       guess_list = shuffle(guess_list);
     }, 'text');
-    
+
 
     // Subscriptions
     session.subscribe('com.google.guesswho.logout', onLogout);
@@ -228,25 +240,25 @@ var Backend = (function () {
     // REGISTER RPC
     //
     session.register('com.google.guesswho.submit', submitGuess).then(
-        function(success){
-           console.log("registered ", success.procedure);
-        }, session.log
+      function(success) {
+        console.log("registered ", success.procedure);
+      }, session.log
     );
     session.register('com.google.guesswho.login', login).then(
-        function(success){
-           console.log("registered ", success.procedure);
-        }, session.log
-     );
+      function(success) {
+        console.log("registered ", success.procedure);
+      }, session.log
+    );
     session.register('com.google.guesswho.changeName', changeName).then(
-        function(success){
-           console.log("registered ", success.procedure);
-        }, session.log
-     );
+      function(success) {
+        console.log("registered ", success.procedure);
+      }, session.log
+    );
   };
 
   return {
 
-    connect: function(){
+    connect: function() {
       var wsuri = null;
 
       // include AutobahnJS
@@ -286,7 +298,7 @@ var Backend = (function () {
       //
       connection.open();
     },
-    
+
   };
 })();
 
--- guess_who/js/mobile.js
+++ guess_who/js/mobile.js
@@ -1,245 +1,272 @@
-var Mobile = (function () {
+var Mobile = (function() {
 
-   //Private variables
-   //
+  //Private variables
+  //
 
-   var session;
-   var user = {id: null, name: "", score: 0};
-   var input_body = $('#input_body');
-   var round_in_progress = false;
-   var timer_interval = null;
+  var session;
+  var user = {
+    id: null,
+    name: "",
+    score: 0
+  };
+  var input_body = $('#input_body');
+  var round_in_progress = false;
+  var timer_interval = null;
 
 
-   // Private functions
-   //
+  // Private functions
+  //
 
-   //set the value of the name container
-   setName = function(new_name){
-      new_name = new_name || user.name // default to user name if no new name is passed
-      // update user name
-      user.name = new_name;
-      // render user name in the name container
-      //
-      $(".name_container").html(new EJS({url: 'templates/user_name.ejs'}).render(user));
-   };
+  //set the value of the name container
+  setName = function(new_name) {
+    new_name = new_name || user.name // default to user name if no new name is passed
+    // update user name
+    user.name = new_name;
+    // render user name in the name container
+    //
+    $(".name_container").html(new EJS({
+      url: 'templates/user_name.ejs'
+    }).render(user));
+  };
 
-   //set the value of the timer
-   setTimer = function(timeout){
-      var timeLeft = function(timeout){
-         var now = new Date();
-         // if we set a timer with a negative or zero time, simply set it to now
-         if (timeout <= 0) timeout = now.getTime();
-         // that way, timeLeft returns 0s instead of a huge negative number
-         return Math.floor((timeout - now.getTime())/1000);      
-      }
-      var renderTimer = function(time_left){      
-         var timer = new EJS({url: 'templates/timer.ejs'}).render({time_left: time_left});
-         $('.timer').html(timer);
-      }
-
-      renderTimer(timeLeft(timeout));
-
-      // Update the timer every second until the timer runs out
-      timer_interval = setInterval(function(){
-         var time_left = timeLeft(timeout);
-         if(time_left <= 0){
-            clearInterval(timer_interval);
-            timer_interval = null;
-            time_left = 0;
-         }
-         renderTimer(time_left);
-      }, 1000);
-   };
-
-   // Handle round start
-   onRoundStart = function(args, kwargs, details){
-      //Populate the input body with buttons
-      var buttons = new EJS({url: 'templates/buttons.ejs'}).render({answers: args});
-      input_body.html(buttons); 
-
-      round_in_progress = true;     
-
-      setTimer(kwargs.round_end);
-   };
-
-   // Handle round end
-   onRoundEnd = function(args, kwargs, details){        
-      round_in_progress = false;
-
-      if(kwargs.round != round) return;
-
-      //Populate the input body with only the correct button
-      var buttons = new EJS({url: 'templates/buttons.ejs'}).render({answers: kwargs.answers});
-      input_body.html(buttons);
-
-      // Disable the button and color it appropriately
-      var answer = input_body.find('.answer:first');
-      answer.prop('disabled', true);
-      answer.addClass('correct');
-
-      // Clear the timer
-      setTimer(0);
-   };
-
-   // Handle new login event
-   onLogins = function(args, kwargs, details){
-      // Update the waiting message
-      var waiting = new EJS({url: 'templates/waiting.ejs'}).render(kwargs); 
-      if (!round_in_progress){
-         input_body.html(waiting);
-      }
-   };
-
-   // Register and subscribe, plus anything else that needs to be done at startup
-   main = function(autobahn_session) {
-
-      session = autobahn_session;
-
-      // Auto logout if the user leaves the page (notify the backend)
-      //
-      $(window).on('beforeunload', function(){
-         session.publish('com.google.guesswho.logout', [user.id]).then(
-            function(success){
-               console.log(success);
-            }
-         );
-      })
-
-      //Check to see if the device already has a user id
-         //Note: needs to use localStorage for 'real' mobile testing
-      user.id = sessionStorage.getItem("id");
-
-      //Log in to the server (and get auto-registered if no uid is present)
-      //
-      session.call("com.google.guesswho.login", [user.id]).then(
-         function(user) {
-            // Store the user object returned from the server  
-            user = user;
-            sessionStorage.setItem("id", user.id);
-            // Display the username
-            setName(user.name);    
-
-            console.log("user is logged in with uid " + user.id + ", and their score is " + user.score);
-            //retry = false;
-         }
-      );
-
-      //
-      // CLICK HANDLERS
-      //
-
-      // Wire up multiple choice buttons
-      //
-      input_body.on('click', '.answer', function(event){
-         
-         // Disable all buttons in the input_body
-         clicked_button = $(event.target);
-         input_body.children('.answer').prop('disabled', true);
-         clicked_button.addClass('selected'); // add a border to indicate that the button has been clicked
-
-         // Submit the answer to the server
-
-         session.call("com.google.guesswho.submit", [], 
-            {id: user.id, val: clicked_button.val(), time: new Date().getTime()}).then(
-            function(success){
-               clicked_button.addClass(success.correct? 'correct' : 'incorrect');
-               // TODO: Display the score in some nice way
-
-               // Update the score
-               user.score += success.score
-               $(".name_container").html(new EJS({url: 'templates/user_name.ejs'}).render(user));
-
-               console.log("Score for this round was ", success.score);
-            },
-            function(error){
-               session.log();
-               //retry
-            }
-         );
-
+  //set the value of the timer
+  setTimer = function(timeout) {
+    var timeLeft = function(timeout) {
+      var now = new Date();
+      // if we set a timer with a negative or zero time, simply set it to now
+      if (timeout <= 0) timeout = now.getTime();
+      // that way, timeLeft returns 0s instead of a huge negative number
+      return Math.floor((timeout - now.getTime()) / 1000);
+    }
+    var renderTimer = function(time_left) {
+      var timer = new EJS({
+        url: 'templates/timer.ejs'
+      }).render({
+        time_left: time_left
       });
+      $('.timer').html(timer);
+    }
 
-      // Wire up the name container for setting new user names
-      $(".name_container").on('click', function(event){
-         var container = $(event.target);
-         
-         // Render the edit_widget with the name
-         var edit_widget = new EJS({url: 'templates/edit_name.ejs'}).render(user);
-         container.html(edit_widget);
-         container.find('input').prop('autofocus', true);
+    renderTimer(timeLeft(timeout));
 
-         // Wire up the edit_widget
-         $("#submit_name").on('click', function(event){
-            var widget = $(event.target);
-            console.log('submit_name clicked');
-            session.call("com.google.guesswho.changeName", [user.id, $("#edit_name").val()]).then(
-               function(new_name){
-                  console.log(new_name);
-                  setName(new_name);
-               },
-               function(error){
-                  setName(); //re render with cached name
-               }
-            )
-         })
+    // Update the timer every second until the timer runs out
+    timer_interval = setInterval(function() {
+      var time_left = timeLeft(timeout);
+      if (time_left <= 0) {
+        clearInterval(timer_interval);
+        timer_interval = null;
+        time_left = 0;
+      }
+      renderTimer(time_left);
+    }, 1000);
+  };
+
+  // Handle round start
+  onRoundStart = function(args, kwargs, details) {
+    //Populate the input body with buttons
+    var buttons = new EJS({
+      url: 'templates/buttons.ejs'
+    }).render({
+      answers: args
+    });
+    input_body.html(buttons);
+
+    round_in_progress = true;
+
+    setTimer(kwargs.round_end);
+  };
+
+  // Handle round end
+  onRoundEnd = function(args, kwargs, details) {
+    round_in_progress = false;
+
+    if (kwargs.round != round) return;
+
+    //Populate the input body with only the correct button
+    var buttons = new EJS({
+      url: 'templates/buttons.ejs'
+    }).render({
+      answers: kwargs.answers
+    });
+    input_body.html(buttons);
+
+    // Disable the button and color it appropriately
+    var answer = input_body.find('.answer:first');
+    answer.prop('disabled', true);
+    answer.addClass('correct');
+
+    // Clear the timer
+    setTimer(0);
+  };
+
+  // Handle new login event
+  onLogins = function(args, kwargs, details) {
+    // Update the waiting message
+    var waiting = new EJS({
+      url: 'templates/waiting.ejs'
+    }).render(kwargs);
+    if (!round_in_progress) {
+      input_body.html(waiting);
+    }
+  };
+
+  // Register and subscribe, plus anything else that needs to be done at startup
+  main = function(autobahn_session) {
+
+    session = autobahn_session;
+
+    // Auto logout if the user leaves the page (notify the backend)
+    //
+    $(window).on('beforeunload', function() {
+      session.publish('com.google.guesswho.logout', [user.id]).then(
+        function(success) {
+          console.log(success);
+        }
+      );
+    })
+
+    //Check to see if the device already has a user id
+    //Note: needs to use localStorage for 'real' mobile testing
+    user.id = sessionStorage.getItem("id");
+
+    //Log in to the server (and get auto-registered if no uid is present)
+    //
+    session.call("com.google.guesswho.login", [user.id]).then(
+      function(user) {
+        // Store the user object returned from the server  
+        user = user;
+        sessionStorage.setItem("id", user.id);
+        // Display the username
+        setName(user.name);
+
+        console.log("user is logged in with uid " + user.id + ", and their score is " + user.score);
+        //retry = false;
+      }
+    );
+
+    //
+    // CLICK HANDLERS
+    //
+
+    // Wire up multiple choice buttons
+    //
+    input_body.on('click', '.answer', function(event) {
+
+      // Disable all buttons in the input_body
+      clicked_button = $(event.target);
+      input_body.children('.answer').prop('disabled', true);
+      clicked_button.addClass('selected'); // add a border to indicate that the button has been clicked
+
+      // Submit the answer to the server
+
+      session.call("com.google.guesswho.submit", [], {
+        id: user.id,
+        val: clicked_button.val(),
+        time: new Date().getTime()
+      }).then(
+        function(success) {
+          clicked_button.addClass(success.correct ? 'correct' : 'incorrect');
+          // TODO: Display the score in some nice way
+
+          // Update the score
+          user.score += success.score
+          $(".name_container").html(new EJS({
+            url: 'templates/user_name.ejs'
+          }).render(user));
+
+          console.log("Score for this round was ", success.score);
+        },
+        function(error) {
+          session.log();
+          //retry
+        }
+      );
+
+    });
+
+    // Wire up the name container for setting new user names
+    $(".name_container").on('click', function(event) {
+      var container = $(event.target);
+
+      // Render the edit_widget with the name
+      var edit_widget = new EJS({
+        url: 'templates/edit_name.ejs'
+      }).render(user);
+      container.html(edit_widget);
+      container.find('input').prop('autofocus', true);
+
+      // Wire up the edit_widget
+      $("#submit_name").on('click', function(event) {
+        var widget = $(event.target);
+        console.log('submit_name clicked');
+        session.call("com.google.guesswho.changeName", [user.id, $("#edit_name").val()]).then(
+          function(new_name) {
+            console.log(new_name);
+            setName(new_name);
+          },
+          function(error) {
+            setName(); //re render with cached name
+          }
+        )
       })
+    })
 
-      //
-      // SUBSCRIPTIONS
-      //
-     
-      // Subscribe to Round Start event
-      //
-      session.subscribe("com.google.guesswho.roundStart", onRoundStart).then(
-         function(success){
-            console.log("subscribed to ", success.topic);
-         }, session.log
-      );
+    //
+    // SUBSCRIPTIONS
+    //
 
-      // Subscribe to Round End event
-      //
-       session.subscribe("com.google.guesswho.roundEnd", onRoundEnd).then(
-         function(success){
-            console.log("subscribed to ", success.topic);
-         }, session.log
-      );   
+    // Subscribe to Round Start event
+    //
+    session.subscribe("com.google.guesswho.roundStart", onRoundStart).then(
+      function(success) {
+        console.log("subscribed to ", success.topic);
+      }, session.log
+    );
 
-      // Subscribe to Logins event
-      //
-      session.subscribe("com.google.guesswho.newLogin", onLogins).then(
-         function(success){
-            console.log("subscribed to ", success.topic);
-         }, session.log
-      );
-   };
+    // Subscribe to Round End event
+    //
+    session.subscribe("com.google.guesswho.roundEnd", onRoundEnd).then(
+      function(success) {
+        console.log("subscribed to ", success.topic);
+      }, session.log
+    );
+
+    // Subscribe to Logins event
+    //
+    session.subscribe("com.google.guesswho.newLogin", onLogins).then(
+      function(success) {
+        console.log("subscribed to ", success.topic);
+      }, session.log
+    );
+  };
 
   return {
 
-   // A public function utilizing privates
-   connect: function(){
+    // A public function utilizing privates
+    connect: function() {
       // the URL of the WAMP Router (Crossbar.io)
       //
       var wsuri = null;
       if (document.location.origin == "file://") {
-         wsuri = "ws://127.0.0.1:8080/ws";
+        wsuri = "ws://127.0.0.1:8080/ws";
 
       } else {
-         wsuri = (document.location.protocol === "http:" ? "ws:" : "wss:") + "//" +
-            document.location.host + "/ws";
+        wsuri = (document.location.protocol === "http:" ? "ws:" : "wss:") + "//" +
+          document.location.host + "/ws";
       }
 
       // the WAMP connection to the Router
       //
       var connection = new autobahn.Connection({
-         url: wsuri,
-         realm: "realm1"
+        url: wsuri,
+        realm: "realm1"
       });
 
       // fired when connection is established and session attached
       //
       connection.onopen = function(session, details) {
 
-         main(session);
+        main(session);
 
       };
 
@@ -247,14 +274,14 @@ var Mobile = (function () {
       //
       connection.onclose = function(reason, details) {
 
-         console.log("Connection lost: " + reason);
+        console.log("Connection lost: " + reason);
 
       }
 
       // now actually open the connection
       //
       connection.open();
-   }
+    }
   };
 
 })();
