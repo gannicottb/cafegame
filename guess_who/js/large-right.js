8c816f5a65d3bea838c3c6c644aedccc64bfab2a
Brandon Gannicott <brandon.gannicott@gmail.com>
2014-10-12 21:41:51 -0700

    Using a WebRTC hack to get local IP to generate the QR code for our display

--- guess_who/js/large-right.js
+++ guess_who/js/large-right.js
@@ -1,3 +1,53 @@
+var loadQRCode = function(){
+  // Courtesy of http://net.ipcalf.com/
+
+  // NOTE: window.RTCPeerConnection is "not a constructor" in FF22/23
+  var RTCPeerConnection = /*window.RTCPeerConnection ||*/ window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+  
+  if (RTCPeerConnection){
+    var rtc = new RTCPeerConnection({iceServers:[]});      
+
+    if (window.mozRTCPeerConnection) {      // FF needs a channel/stream to proceed
+        rtc.createDataChannel('', {reliable:false});
+    };
+    
+    rtc.onicecandidate = function (evt) {
+        if (evt.candidate) grepSDP(evt.candidate.candidate);      
+    };
+
+    rtc.createOffer(function (offerDesc) {
+        grepSDP(offerDesc.sdp);        
+        rtc.setLocalDescription(offerDesc);        
+    }, function (e) { console.warn("offer failed", e); });     
+
+    var addrs = Object.create(null);
+    addrs["0.0.0.0"] = false;
+    function updateDisplay(newAddr) {
+        if (newAddr in addrs) return;
+        else addrs[newAddr] = true;
+        var displayAddrs = Object.keys(addrs).filter(function (k) { return addrs[k]; });
+        // document.getElementById('list').textContent = displayAddrs.join(" or perhaps ") || "n/a";
+        qrcode.makeCode("http://"+displayAddrs[0]+":8080/mobile.html");
+    }
+
+    function grepSDP(sdp) {       
+      var hosts = [];
+      sdp.split('\r\n').forEach(function (line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
+          if (~line.indexOf("a=candidate")) {     // http://tools.ietf.org/html/rfc4566#section-5.13
+              var parts = line.split(' '),        // http://tools.ietf.org/html/rfc5245#section-15.1
+                  addr = parts[4],
+                  type = parts[7];
+              if (type === 'host') updateDisplay(addr);
+          } else if (~line.indexOf("c=")) {       // http://tools.ietf.org/html/rfc4566#section-5.7
+              var parts = line.split(' '),
+                  addr = parts[2];
+              updateDisplay(addr);
+          }
+      });
+    };       
+  }  
+}
+
 // the URL of the WAMP Router (Crossbar.io)
 //
 var wsuri;
@@ -25,15 +75,19 @@ connection.onopen = function(session, details) {
 };
 
 var loggedInUsers = [];
+var qrcode;
 
 function main(session) {
 
   //Display all logged in users
   //showAllLoggedInUsers();
 
+  //Find local IP and display QR Code  
+  loadQRCode();
+
   //Display the QR code
-  new QRCode($("#qr_code")[0], {
-      text: 'http://10.0.0.11:8080/mobile.html',
+  qrcode = new QRCode($("#qr_code")[0], {
+      text: 'http://127.0.0.1:8080/mobile.html',
       width: 256,
       height: 256,
       colorDark : "#000000",
